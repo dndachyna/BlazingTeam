a701d2eb0679dd0ecd6be8fdff1feae15aa1ee2c
dndachyna <143949168+dndachyna@users.noreply.github.com>
2023-09-15 02:10:40 +0700

    Add files via upload

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prjct Boxity</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="nav">
+            <div class="logo">
+                <a href="#">
+                    <p>Mr.Seblak</p>
+                </a>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div class="header">
+            <h1>Mr.<span>Seblak</span></h1>
+        </div>
+
+        <div class="content-header">
+            <p>Mr.Seblak ialah sebuah usaha makanan khas sunda yang sangat populer dikalangan masyarakat Indonesia, 
+               yang berisikan kerupuk, makaroni, bakso daging, sosis, telur, mie, ceker serta beberapa makanan instan lainnya</p>
+            <br>
+            <input type="text" name="text" class="search-input"
+            placeholder="cari seblak favorit mu...">
+            <input class="search-btn" type="button" value="cari">
+        </div>
+
+        <div class="col-img">
+            <img src="img/" alt="">
+        </div>
+
+     </div>
+</body>
+</html>
